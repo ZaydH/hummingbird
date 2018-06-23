df8835d78c7f0d77b3976e9cbfa9ef8ba685960c
Zayd Hammoudeh <zhammoud@ucsc.edu>
2018-06-22 21:06:01 -0700

    Improved logging of the HB architecture class. Made architecture class PEP8 compliant.

--- python_simplified/command_line/processcall.py
+++ python_simplified/command_line/processcall.py
@@ -7,69 +7,72 @@ from enum import Enum
 
 
 class RunStatus(Enum):
-  PASSED = "passed"
-  TIMEOUT = "timeout"
+    PASSED = "passed"
+    TIMEOUT = "timeout"
+
 
 class CommandLine(object):
 
-  @staticmethod
-  def _wait_timeout(proc, seconds):
-    """
-    Process Timeout Wait Function
+    @staticmethod
+    def _wait_timeout(proc, seconds):
+        """
+        Process Timeout Wait Function
 
-    Waits for a function to either complete execution.  If the program takes
-    longer to complete than the timeout, it kills the process.
+        Waits for a function to either complete execution.  If the program takes
+        longer to complete than the timeout, it kills the process.
 
-    :param proc: Process information
+        :param proc: Process information
 
-    :param seconds: Timeout time in seconds
-    :type seconds: float
+        :param seconds: Timeout time in seconds
+        :type seconds: float
 
-    :return: Test status result
-    :rtype: Test.Result
-    """
-    """Wait for a process to finish, or raise exception after timeout"""
-    start = time.time()
-    end = start + seconds
-    interval = 0.01
+        :return: Test status result
+        :rtype: Test.Result
+        """
 
-    while True:
-      result = proc.poll()
-      if result is not None:
-        return RunStatus.PASSED
-      if time.time() >= end:
-        os.killpg(proc.pid, signal.SIGTERM)
-        # raise RuntimeError("Process timed out")
-        return RunStatus.TIMEOUT
-      time.sleep(interval)
+        """Wait for a process to finish, or raise exception after timeout"""
+        start = time.time()
+        end = start + seconds
+        interval = 0.01
 
-  @staticmethod
-  def run(command_line_str, timeout_sec, **kwargs):
-    """
-    Execute the command line command.
+        while True:
+            result = proc.poll()
+            if result is not None:
+                return RunStatus.PASSED
+            if time.time() >= end:
+                os.killpg(proc.pid, signal.SIGTERM)
+                # raise RuntimeError("Process timed out")
+                return RunStatus.TIMEOUT
+            time.sleep(interval)
 
-    :param command_line_str: Instruction with command line.  The string should have all parameters
-                             and be self contained.  Processing the string will be handled by the
-                             function.
-    :type command_line_str: str
-    :param timeout_sec: Timeout for the command line instruction.
-    :type timeout_sec: int
-    :param kwargs:
-    :return: Result from the run and the command line instruction.
-    :rtype: Tuple(RunStatus, str)
-    """
-    if 'stdout' in kwargs:
-      raise ValueError('stdout argument not allowed.')
-    inputs_args = shlex.split(command_line_str)
-    process = subprocess.Popen(inputs_args, stdout=subprocess.PIPE,
-                               preexec_fn=os.setsid, **kwargs)
-    result = CommandLine._wait_timeout(process, timeout_sec)
+    @staticmethod
+    def run(command_line_str, timeout_sec, **kwargs):
+        """
+        Execute the command line command.
 
-    if result == RunStatus.TIMEOUT:
-      return result, None
+        :param command_line_str: Instruction with command line.  The string
+                                 should have all parameters and be self
+                                 contained.  Processing the string will be
+                                 handled by the function.
+        :type command_line_str: str
+        :param timeout_sec: Timeout for the command line instruction.
+        :type timeout_sec: int
+        :param kwargs:
+        :return: Result from the run and the command line instruction.
+        :rtype: Tuple(RunStatus, str)
+        """
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed.')
+        inputs_args = shlex.split(command_line_str)
+        process = subprocess.Popen(inputs_args, stdout=subprocess.PIPE,
+                                   preexec_fn=os.setsid, **kwargs)
+        result = CommandLine._wait_timeout(process, timeout_sec)
 
-    # Read the output of the process
-    output, unused_err = process.communicate()
-    if output is not None and type(output) != str:
-      output = output.decode()  # For python3 compatibility in bash
-    return result, output
+        if result == RunStatus.TIMEOUT:
+            return result, None
+
+        # Read the output of the process
+        output, unused_err = process.communicate()
+        if output is not None and type(output) != str:
+            output = output.decode()  # For python3 compatibility in bash
+        return result, output
--- python_simplified/mpi/hb_framework.py
+++ python_simplified/mpi/hb_framework.py
@@ -9,120 +9,136 @@ from mpi.message import ControllerToWorkerMessage
 
 
 class HummingbirdFramework(object):
-  """
-  Framework that the user runs to run on Hummingbird.
-  """
-
-  """
-  File to write the logs for the entire Hummingbird framework.
-  """
-  log_file = "hb_framework.log"
-  """
-  Logging level when printing messages.
-  """
-  log_level = logging.DEBUG
-
-  @staticmethod
-  def run(TaskClass, WorkerClass):
     """
-    Run by the user.  It manages all communication operations that must be done.  It only leaves
-    for the user to define what tasks must be performed.
-
-    :param TaskClass: Defines the task sent to the workers.
-    :type TaskClass: class
-
-    :param WorkerClass: Class that defines how the workers execute the passed task.
-    :type WorkerClass: class
+    Framework that the user runs to run on Hummingbird.
     """
-    HummingbirdFramework._setup_logger()
 
-    # Rank selects whether the task is a master or slave.
-    rank = MPI.COMM_WORLD.Get_rank()
-    comm = MPI.COMM_WORLD
+    """
+    File to write the logs for the entire Hummingbird framework.
+    """
+    log_file = "hb_framework.log"
+    """
+    Logging level when printing messages.
+    """
+    log_level = logging.DEBUG
 
     # noinspection PyPep8Naming
-    MASTER_RANK = 0
-    if rank == MASTER_RANK:
-      logging.info("***************************** New Run Beginning ****************************")
-      HummingbirdFramework._run_controller(comm, TaskClass)
-    else:
-      worker = WorkerClass(comm, rank)
-      worker.run()
+    @staticmethod
+    def run(TaskClass, WorkerClass):
+        """
+        Run by the user.  It manages all communication operations that must be
+        done.  It only leaves for the user to define what tasks must be
+        performed.
 
-  @staticmethod
-  def _is_message_pending(comm):
-    """
-    Waits for a pending message to arrive.
+        :param TaskClass: Defines the task sent to the workers.
+        :type TaskClass: class
 
-    :param comm:
-    :return: Message from the finished worker.  It has the same messa
-    :rtype: dict
-    """
-    return comm.Iprobe(source=MPI.ANY_SOURCE)
+        :param WorkerClass: Class that defines how the workers execute the
+                            passed task.
+        :type WorkerClass: class
+        """
+        # HummingbirdFramework._setup_logger()
 
-  @staticmethod
-  def _run_controller(comm, TaskClass):
-    """
-    Executes the controller and manages sending the messages to the workers.
+        # Rank selects whether the task is a master or slave.
+        rank = MPI.COMM_WORLD.Get_rank()
+        comm = MPI.COMM_WORLD
 
-    :param comm:
-    :param TaskClass: Class that defines the tasks to be sent to the workers.
-    :type TaskClass: class
-    """
-    logging.info('CONTROLLER: Starting {} workers'.format(comm.Get_size() - 1))
-    status = MPI.Status()
-    controller = Controller(comm)
-    solver = TaskClass()
-
-    all_messages_sent = False
-
-    # Run the master
-    while True:
-      while HummingbirdFramework._is_message_pending(comm):
-        worker_result = comm.recv(status=status)
-        controller.add_available_worker(status.Get_source())
-        solver.process_results(worker_result)
-
-      # If all tasks are done, do not exit until
-      if all_messages_sent:
-        if controller.all_workers_completed():
-          logging.info("CONTROLLER: All workers done and processed. Exiting...")
-          sys.exit(0)
-        time.sleep(1)
-        continue
-
-      while controller.have_available_workers_p():
-        try:
-          task = solver.get_next()
-        except StopIteration:
-          # Generator fully consumed
-          controller.terminate_everything()
-          all_messages_sent = True
-          break
+        # noinspection PyPep8Naming
+        MASTER_RANK = 0
+        if rank == MASTER_RANK:
+            logging.info("***************** New Run Beginning ****************")
+            HummingbirdFramework._run_controller(comm, TaskClass)
         else:
-          worker = controller.get_available_worker()
-          logging.info('CONTROLLER: Packing problem: {}'.format(task))
-          message = ControllerToWorkerMessage.build(False, task)
-          comm.isend(message, dest=worker)
-          time.sleep(1)
+            worker = WorkerClass(comm, rank)
+            worker.run()
 
-  @staticmethod
-  def _setup_logger():
-    """
-    Logger Configurator
+    @staticmethod
+    def _is_message_pending(comm):
+        """
+        Waits for a pending message to arrive.
 
-    Configures the framework's logger.
-    """
-    date_format = '%m/%d/%Y %I:%M:%S %p'  # Example Time Format - 12/12/2010 11:46:36 AM
+        :param comm:
+        :return: Message from the finished worker.
+        :rtype: dict
+        """
+        return comm.Iprobe(source=MPI.ANY_SOURCE)
 
-    logging.basicConfig(filename=HummingbirdFramework.log_file,
-                        level=HummingbirdFramework.log_level,
-                        format='%(asctime)s -- %(levelname)s -- %(message)s', datefmt=date_format)
+    # noinspection PyPep8Naming
+    @staticmethod
+    def _run_controller(comm, TaskClass):
+        """
+        Executes the controller and manages sending the messages to the workers.
 
-    # Also print to stdout
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s -- %(levelname)s -- %(message)s')
-    handler.setFormatter(formatter)
-    logging.getLogger().addHandler(handler)
+        :param comm:
+        :param TaskClass: Class that defines the tasks to be sent to the
+                          workers.
+        :type TaskClass: class
+        """
+        log_txt = 'CONTROLLER: Starting {} workers'.format(comm.Get_size() - 1)
+        logging.info(log_txt)
+        status = MPI.Status()
+        controller = Controller(comm)
+        solver = TaskClass()
 
+        all_messages_sent = False
+
+        # Run the master
+        while True:
+            while HummingbirdFramework._is_message_pending(comm):
+                worker_result = comm.recv(status=status)
+                controller.add_available_worker(status.Get_source())
+                solver.process_results(worker_result)
+
+            # If all tasks are done, do not exit until
+            if all_messages_sent:
+                if controller.all_workers_completed():
+                    logging.info("CONTROLLER: All workers done and " 
+                                 "processed. Exiting...")
+                    sys.exit(0)
+                time.sleep(1)
+                continue
+
+            while controller.have_available_workers_p():
+                try:
+                    task = solver.get_next()
+                except StopIteration:
+                    # Generator fully consumed
+                    controller.terminate_everything()
+                    all_messages_sent = True
+                    break
+                else:
+                    worker = controller.get_available_worker()
+                    logging.info("CONTROLLER: Packing task \"%s\" for worker %d"
+                                 % (task, worker))
+                    message = ControllerToWorkerMessage.build(False, task)
+                    comm.isend(message, dest=worker)
+                    time.sleep(1)
+
+    @staticmethod
+    def _setup_logger():
+        """
+        Logger Configurator
+
+        Configures the framework's logger.
+        """
+        # noinspection PyProtectedMember
+        num_existing_handlers = len(logging._handlers)
+        if num_existing_handlers > 0:
+            logging.info("%d handlers already exist. No loggers to be added"
+                         % num_existing_handlers)
+            return
+
+        # Example Time Format - 12/12/2010 11:46:36 AM
+        date_format = '%m/%d/%Y %I:%M:%S %p'
+
+        format_str = '%(asctime)s -- %(levelname)s -- %(message)s'
+        logging.basicConfig(filename=HummingbirdFramework.log_file,
+                            level=HummingbirdFramework.log_level,
+                            format=format_str, datefmt=date_format)
+
+        # Also print to stdout
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(format_str)
+        handler.setFormatter(formatter)
+        logging.getLogger().addHandler(handler)
